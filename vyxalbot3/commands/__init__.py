79a5cac906aee9cba9bba7be7a51d9970cdf54af
Ginger <75683114+gingershaped@users.noreply.github.com>
2024-12-04 15:54:45 +0000

    Implement !!/help

--- vyxalbot3/commands/__init__.py
+++ vyxalbot3/commands/__init__.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, EnumType
 import inspect
 from logging import getLogger
 from typing import TYPE_CHECKING, Awaitable, Callable
@@ -24,6 +24,7 @@ ARGUMENT_TYPE_SIGNATURES = {
 
 PREFIX = "!!/"
 
+
 class Commands:
     logger = getLogger("commands")
 
@@ -32,9 +33,9 @@ class Commands:
         self.commands: dict[str, CommandTree] = {}
 
         for method_name, method in inspect.getmembers(self, inspect.ismethod):
-            if method_name.startswith("__"):
+            if not method_name.endswith("_command"):
                 continue
-            path = method_name.split("_")
+            path = method_name.removesuffix("_command").split("_")
             parent: dict[str, CommandTree] = self.commands
             while len(path) > 1:
                 node = path.pop(0)
@@ -76,10 +77,10 @@ class Commands:
                 break
             if argument[1] not in command:
                 if index == 0:
-                    return f"There is no command named \"{argument[1]}\"."
+                    return f"There is no command named !!/{argument[1]}."
                 parent_name = " ".join(a[1] for a in arguments[:index] if a[0] == ArgumentType.FLAG)
                 return (
-                    f"The command \"{parent_name}\" has no subcommand named \"{argument[1]}\". "
+                    f"The group !!/{parent_name} has no subcommand named \"{argument[1]}\". "
                     f"Its subcommands are: {", ".join(command.keys())}"
                 )
             command = command[argument[1]]
@@ -88,14 +89,14 @@ class Commands:
                 break
         if isinstance(command, dict):
             parent_name = " ".join(a[1] for a in arguments if a[0] == ArgumentType.FLAG)
-            return f"Subcommands of \"{parent_name}\" are: {", ".join(command.keys())}"
+            return f"Subcommands of !!/{parent_name} are: {", ".join(command.keys())}"
         
         argument_values = []
         parameters = inspect.signature(command).parameters
-        for argument_name, parameter in parameters.items():
-            if argument_name in ("self", "event"):
+        for name, parameter in parameters.items():
+            if name in ("self", "event"):
                 continue
-            if issubclass(parameter.annotation, Enum):
+            if isinstance(parameter.annotation, EnumType):
                 expected_type = ArgumentType.FLAG
             else:
                 expected_type = ARGUMENT_TYPE_SIGNATURES[parameter.annotation]
@@ -104,22 +105,58 @@ class Commands:
                     case (ArgumentType.ERROR, message):
                         return f"Parsing error: {message}"
                     case (ArgumentType.FLAG, name) if expected_type == ArgumentType.FLAG:
-                        assert issubclass(parameter.annotation, Enum)
+                        assert isinstance(parameter.annotation, EnumType)
                         argument_values.append(parameter.annotation(name))
                     case (argument_type, value) if expected_type == argument_type:
                         argument_values.append(value)
                     case (actual_type, _):
                         return (
-                            f"Incorrect type supplied for argument `{argument_name}`; "
+                            f"Incorrect type supplied for argument `{name}`; "
                             f"expected **{expected_type.name}** but got **{actual_type.name}**"    
                         )
             elif parameter.default is not parameter.empty:
                 argument_values.append(parameter.default)
             else:
-                return f"Argument `{argument_name}` not provided, expected a value of type **{expected_type.name}**"
+                return f"Argument `{name}` not provided, expected a value of type **{expected_type.name}**"
         if "event" in parameters:
             return await command(event, *argument_values)
         return await command(*argument_values)
 
-    async def hello(self, name: str):
-        return f"hello {name}"
+    async def help_command(self, name: str):
+        """Display parameters and help for a command."""
+        if name == "me":
+            return "I'd love to, but I don't have any limbs."
+        path = name.split(" ")
+        help_target = self.commands
+        for index, segment in enumerate(path):
+            if segment not in help_target:
+                if index == 0:
+                    return f"There is no command named \"{segment}\"."
+                parent_name = " ".join(path[:index])
+                return (
+                    f"The group \"{parent_name}\" has no subcommand named \"{segment}\". "
+                    f"Its subcommands are: {", ".join(help_target.keys())}"
+                )
+            help_target = help_target[segment]
+            if not isinstance(help_target, dict):
+                break
+        if isinstance(help_target, dict):
+            parent_name = " ".join(path)
+            return f"Subcommands of !!/{parent_name} are: {", ".join(help_target.keys())}"
+        
+        doc = help_target.__doc__ if help_target.__doc__ is not None else "(no help)"
+        parameters = []
+        for parameter_name, parameter in inspect.signature(help_target).parameters.items():
+            if name in ("self", "event"):
+                continue
+            if isinstance(parameter.annotation, EnumType):
+                values = "/".join(item.value for item in list(parameter.annotation) if isinstance(item, Enum))
+                body = f"ENUM {parameter_name}: {values}"
+            else:
+                body = f"{ARGUMENT_TYPE_SIGNATURES[parameter.annotation].name} {parameter_name}"
+            if parameter.default != parameter.empty:
+                default = parameter.default.value if isinstance(parameter.default, Enum) else repr(parameter.default)
+                parameters.append(f"[{body} = {default}]")
+            else:
+                parameters.append(f"({body})")
+        return f"`!!/{name} {" ".join(parameters)}`: {doc}"
