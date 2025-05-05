b7f521a1b596b24e868ad86348b2ca17fb392275
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-05-05 09:57:39 -0400

    Use returncache module instead of custom decorator

--- vyxalbot3/github/__init__.py
+++ vyxalbot3/github/__init__.py
@@ -5,8 +5,7 @@ from aiohttp import ClientSession
 from gidgethub.aiohttp import GitHubAPI
 from gidgethub.apps import get_jwt as get_github_jwt
 
-from vyxalbot3.util import autocache
-
+from returncache import returncache
 
 class AppGitHubAPI(GitHubAPI):
     def __init__(
@@ -16,7 +15,7 @@ class AppGitHubAPI(GitHubAPI):
         self.app_id = app_id
         self.private_key = private_key
 
-    @autocache
+    @returncache(keyed_by_parameters=False)
     async def app_token(self):
         jwt = get_github_jwt(app_id=self.app_id, private_key=self.private_key)
         async for installation in self.getiter("/app/installations", jwt=jwt):
--- vyxalbot3/util.py
+++ vyxalbot3/util.py
@@ -8,42 +8,6 @@ from bs4 import BeautifulSoup, Tag
 from sechat import Server
 from yarl import URL
 
-
-@overload
-def autocache[**P, R](method: Callable[P, tuple[datetime, R]]) -> Callable[P, R]: ...
-
-
-@overload
-def autocache[
-    **P, R
-](method: Callable[P, Coroutine[Any, Any, tuple[datetime, R]]]) -> Callable[
-    P, Coroutine[Any, Any, R]
-]: ...
-
-
-def autocache[
-    **P, R
-](
-    method: Callable[P, tuple[datetime, R] | Coroutine[Any, Any, tuple[datetime, R]]]
-) -> Callable[P, R | Coroutine[Any, Any, R]]:
-    value: tuple[datetime, R] | None = None
-
-    async def _store(ret: Awaitable[tuple[datetime, R]]) -> R:
-        value = await ret
-        return value[1]
-
-    def _wrapper(*args: P.args, **kwargs: P.kwargs):
-        nonlocal value
-        if value is None or value[0] < datetime.now():
-            ret = method(*args, **kwargs)
-            if inspect.isawaitable(ret):
-                return _store(ret)
-            value = ret
-        return value[1]
-
-    return _wrapper
-
-
 MESSAGE_LINK_REGEX = (
     r"https?://chat.stackexchange.com/transcript(/message)?/(?P<id>\d+)(#.*)?"
 )
